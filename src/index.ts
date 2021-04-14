aee7382e5f2e43c81e297aaffc8aae43db07a659
cizionMickey <mickey@cizion.com>
2021-04-14 16:58:16 +0900

    create Element

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ActionsType, app, h, VNode } from "hyperapp";
+import { ActionsType, app, Children, Component, h, VNode } from "hyperapp";
 
 declare global {
   interface Window {
@@ -6,26 +6,82 @@ declare global {
   }
 }
 
+interface Refs<Value> {
+  current: Value;
+}
+interface RyperAttributes {
+  oncreate?: null | ((_el: Element) => void);
+  onupdate?: null | ((_el: Element) => void);
+  ondestroy?: null | ((_el: Element) => void);
+  ref?: null | Refs<any>;
+  [key: string]: any;
+}
+interface RyperComponentResult<State, Actions> {
+  (
+    state?: State,
+    actions?: Actions,
+    props?: RyperAttributes,
+    children?: Array<Children | Children[]>
+  ): VNode<RyperAttributes>;
+}
+interface RyperComponent<State, Actions> extends Component {
+  (
+    attributes: RyperAttributes,
+    children: Array<Children | Children[]>
+  ): RyperComponentResult<State, Actions>;
+}
+
 const React = (() => {
+  let rootActions: any;
+  // const componentRender = () => {};
+  // const elementRender = () => {};
+  // const isComponent = () => {};
+  const createElement = <State, Actions>(
+    type: RyperComponent<State, Actions> | string,
+    props: RyperAttributes,
+    ...children: Array<Children | Children[]>
+  ): RyperComponentResult<State, Actions> => {
+    console.log(type, props, ...children);
+    return () => {
+      return h(type, props, ...children);
+    };
+  };
+  const createActions = <State, Actions>(
+    actions: ActionsType<State, Actions>
+  ): ActionsType<State, Actions> => {
+    return {
+      ...actions,
+      change: () => (state) => ({ ...state }),
+      getState: () => (state) => state,
+    };
+  };
   const init = (view: VNode): VNode => {
     return view;
   };
-
-  const createElement = (type: any, ...props: any) => {
-    console.log(type, props);
-    return h(type, ...props);
-  };
-
   const render = <State, Actions>(
     state: State,
     actions: ActionsType<State, Actions>,
     view: VNode,
     container: Element | null
   ) => {
-    app(state, actions, () => init(view), container);
+    rootActions = app<State, Actions>(
+      state,
+      createActions(actions),
+      () => init(view),
+      container
+    );
+  };
+  const getState = (selector?: (state: any) => any): any => {
+    const state = rootActions.getState();
+    return selector ? selector(state) : state;
+  };
+  const getActions = (selector?: (state: any) => any): any => {
+    return selector ? selector(rootActions) : rootActions;
   };
 
-  return { render, createElement };
+  return { render, createElement, getState, getActions };
 })();
+
+export const { getState, getActions } = React;
 
 export default React;
