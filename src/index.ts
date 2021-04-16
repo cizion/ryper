406a2addc90e9e8d8f31be75e10e2e55270b2137
cizionMickey <mickey@cizion.com>
2021-04-16 15:36:14 +0900

    useState

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,16 @@ interface RyperAttributes {
   ref?: null | Refs<any>;
   [key: string]: any;
 }
+interface V2VNode {
+  type: any;
+  props: any;
+  children: any;
+  node: any;
+  tag: any;
+  key: any;
+  memo?: any;
+  events?: any;
+}
 interface RyperComponentResult<State, Actions> {
   (
     state?: State,
@@ -25,25 +35,96 @@ interface RyperComponentResult<State, Actions> {
   ): VNode<RyperAttributes>;
 }
 interface RyperComponent<State, Actions> extends Component {
-  (
-    attributes: RyperAttributes,
-    children: Array<Children | Children[]>
-  ): RyperComponentResult<State, Actions>;
+  (attributes: RyperAttributes, children: Array<Children | Children[]>):
+    | RyperComponentResult<State, Actions>
+    | V2VNode;
 }
 
 const React = (() => {
   let rootActions: any;
-  // const componentRender = () => {};
-  // const elementRender = () => {};
-  // const isComponent = () => {};
+
+  let hookIdx = 0;
+  let hooks: Array<any> = [];
+  let hook: Array<any> = [];
+
+  const isUndefined = <Value>(value: Value) => {
+    return value === undefined;
+  }
+  const isV2VNode = (elFn: V2VNode | any): elFn is V2VNode => {
+    const V2NodeKeys = ["type", "props", "children", "node", "tag", "key"];
+    return (
+      typeof elFn === "object" && V2NodeKeys.every((key) => Object.keys(key))
+    );
+  };
+  const isRyperComponentResult = <State, Actions>(
+    elFn: RyperComponentResult<State, Actions> | V2VNode | any
+  ): elFn is RyperComponentResult<State, Actions> => {
+    return typeof elFn === "function";
+  };
+  const isRyperComponent = <State, Actions>(
+    type: RyperComponent<State, Actions> | string
+  ): type is RyperComponent<State, Actions> => {
+    return typeof type === "function";
+  };
+  const getVNode = <State, Actions>(
+    type: RyperComponent<State, Actions>,
+    props: RyperAttributes,
+    children: Array<Children | Children[]>
+  ): VNode<RyperAttributes> => {
+    let elFn: RyperComponentResult<State, Actions> | V2VNode | any = type(
+      props,
+      children
+    );
+    let el: VNode<RyperAttributes>;
+    if (isRyperComponentResult(elFn)) {
+      el = elFn();
+    } else if (isV2VNode(elFn)) {
+      const { key, children, tag, props } = elFn;
+      el = {
+        nodeName: tag,
+        attributes: props,
+        children,
+        key,
+      };
+    } else {
+      el = elFn;
+    }
+    return el;
+  };
+  const componentRender = <State, Actions>(
+    type: RyperComponent<State, Actions>,
+    props: RyperAttributes,
+    children: Array<Children | Children[]>
+  ): VNode<RyperAttributes> => {
+    let el;
+    try {
+      el = getVNode(type, props, children);
+    } catch (error) {
+      el = getVNode(type, props, children);
+    }
+    return el;
+  };
+  const elementRender = <State, Actions>(
+    type: RyperComponent<State, Actions> | string,
+    props: RyperAttributes,
+    children: Array<Children | Children[]>
+  ): VNode<RyperAttributes> => {
+    const el = h(type, props, ...children);
+
+    return el;
+  };
   const createElement = <State, Actions>(
     type: RyperComponent<State, Actions> | string,
     props: RyperAttributes,
     ...children: Array<Children | Children[]>
   ): RyperComponentResult<State, Actions> => {
-    console.log(type, props, ...children);
-    return () => {
-      return h(type, props, ...children);
+    return (...params) => {
+      const [, , addProps = {}, addChildren = []] = params;
+      const newProps = { ...props, ...addProps };
+      const newChildren = [...children, ...addChildren];
+      return isRyperComponent(type)
+        ? componentRender(type, newProps, newChildren)
+        : elementRender(type, newProps, newChildren);
     };
   };
   const createActions = <State, Actions>(
@@ -71,6 +152,17 @@ const React = (() => {
       () => init(view),
       container
     );
+  };
+  const useState = <Value>(initValue: Value):[value: Value, setState: (newValue: Value) => Value]  => {
+    const _idx = hookIdx;
+
+    const setState = (newValue: Value, falg = true) => {
+      
+    }
+
+    const state = isUndefined() ? : 
+
+    return [initValue, setState];
   };
   const getState = (selector?: (state: any) => any): any => {
     const state = rootActions.getState();
